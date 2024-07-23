d186d5b2e6b1bb24d8bb047739156c061a2bcf4d
Dovalkin <tranquangtuananh2801@gmail.com>
2024-07-23 16:46:40 +1000

    update mainmenu

--- Assets/Scripts/MenuControls.cs
+++ Assets/Scripts/MenuControls.cs
@@ -2,19 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MenuControls : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public GameObject chooseGameScreen;
+    public GameObject title;
+    public Slider slider;
+
+    public TMP_Text progressText;
+
+
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         CollectableControl.pointsCount = 0;
         Time.timeScale = 1;
     }
-    public void StartGame()
+    public void LoadGame(int sceneIndex)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadAsynchronously(sceneIndex));
         Time.timeScale = 1;
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        title.SetActive(false);
+
+        loadingScreen.SetActive(true);
+
+        chooseGameScreen.SetActive(false);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            progressText.text = (progress * 100f).ToString() + "%";
+
+            yield return null;
+        }
     }
     
     public void BackToMainMenu()
